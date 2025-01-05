8c3834cca5dc6eec5b06f0cc0d67df86a2ed58e3
Pavan shelke <pavanshelke16@gmail.com>
2025-01-06 02:46:55 +0530

    portfolio

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = 'nv)=_h7=(#+dn7hk2#n17v406lzs#2-)l8kepd#y(u2g)g5_%-'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
